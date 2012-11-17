8d72f32674461a79e718ac2dc114f2e4d4422f83
Chad Burggraf <chad.burggraf@gmail.com>
2012-11-17 10:37:08 -0700

    Set up test project. Create initial nuspec file.

---
+++ Source/Parlay.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Tasty Codes">
+//     Copyright (c) 2012 Chad Burggraf.
+// </copyright>
+//-----------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Parlay Tests")]
+[assembly: AssemblyDescription("Parlay test suite.")]
+[assembly: Guid("c577e049-290a-4934-93cb-79cb9a5282dc")]
